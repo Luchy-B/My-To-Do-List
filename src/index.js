8e73f29625abbdfb50c8cb62038667af514eef4e
Luchy-B <okparabon@gmail.com>
2023-02-25 03:47:00 +0100

    Update linters error

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import './style.css';
 
 const taskArray = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+const inputList = document.getElementById('input-List');
 
-const createItem = (item = { task: item.value, index: taskArray.length, completed: false }) => {
+const createItem = (item = {
+  task: inputList.value,
+  index: taskArray.length,
+  completed: false,
+}) => {
   taskArray.push(item);
   localStorage.setItem('tasks', JSON.stringify(taskArray));
   window.location.reload();
 };
 
-const inputList = document.getElementById('input-List');
 inputList.value = '';
 inputList.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
-    const item = document.querySelector('#input-List');
-    createItem(item.value);
+    createItem(inputList.value);
   }
 });
 
@@ -24,7 +27,7 @@ function displayItems() {
     <div class="input-Content">
         <input type="checkbox" class="complete" name="completed" />
         <span disabled>${taskArray[i]}</span>
-    </div>`;
+        </div>`;
   }
   document.querySelector('#todo-Collections').innerHTML = tasks;
 }
